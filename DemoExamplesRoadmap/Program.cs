aa3261327ef80e30766f60fe113abb3e85b44826
d.shchebetovskyi <d.shchebetovskyi@OD-MOBILE-05>
2021-03-01 13:08:12 +0200

    Add project files.

---
+++ DemoExamplesRoadmap/Program.cs
@@ -0,0 +1,21 @@
+﻿using DemoExamplesRoadmap.Classes;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+
+namespace DemoExamplesRoadmap
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
